f0a79e5ec8baed1d6c2b50002e640e9cd28e5d11
Shivanishet <shivanishet035@gmail.com>
2025-10-03 19:12:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Quiz from './components/Quiz/quiz'
+
+const App = () => {
+  return (
+    <>
+      <Quiz />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+// data.js
+export const data = [
+  {
+    question: "Which is the largest continent in the world?",
+    option1: "Africa",
+    option2: "Asia",
+    option3: "Europe",
+    option4: "Antarctica",
+    answer: 2,
+  },
+  {
+    question: "Which is the longest river in the world?",
+    option1: "Nile",
+    option2: "Amazon",
+    option3: "Yangtze",
+    option4: "Ganga",
+    answer: 1,
+  },
+  {
+    question: "Which country has the largest population?",
+    option1: "India",
+    option2: "USA",
+    option3: "China",
+    option4: "Russia",
+    answer: 3,
+  },
+  {
+    question: "Which is the smallest country in the world?",
+    option1: "Monaco",
+    option2: "Vatican City",
+    option3: "Malta",
+    option4: "Liechtenstein",
+    answer: 2,
+  },
+  {
+    question: "Which is the largest desert in the world?",
+    option1: "Sahara",
+    option2: "Gobi",
+    option3: "Kalahari",
+    option4: "Arctic Desert",
+    answer: 4,
+  },
+];
